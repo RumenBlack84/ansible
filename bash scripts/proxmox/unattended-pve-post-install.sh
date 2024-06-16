da430c8baa298ecfe37fc3c6416192714412cdc8
RumenBlack84 <80719678+RumenBlack84@users.noreply.github.com>
2024-06-16 15:53:09 -0400

    Update unattended-pve-post-install.sh

--- bash scripts/proxmox/unattended-pve-post-install.sh
+++ bash scripts/proxmox/unattended-pve-post-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#https://raw.githubusercontent.com/RumenBlack84/ansible/main/unattended-pve-post-install.sh
-#Blatantly forked and refactored by Brian Grant (and chatgpt) to support non-interative install for use in ansible automation
+# https://raw.githubusercontent.com/RumenBlack84/ansible/main/unattended-pve-post-install.sh
+# Blatantly forked and refactored by Brian Grant (and chatgpt) to support non-interactive install for use in ansible automation
 
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
